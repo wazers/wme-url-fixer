69f91654b45f97a7414952286f63044c8fe75969
Glodenox <github@tomputtemans.com>
2017-09-03 15:36:55 +0200

    Prevent potential infinite page reloading (just to be safe) Stricter definition on where to apply the fix in the URL (also just to be safe)

--- wme-url-fixer.user.js
+++ wme-url-fixer.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        WME URL Fixer
 // @namespace   http://www.tomputtemans.com/
-// @description Temporary workaround to get your userscripts working again if they are missing the trailing slash in the URL
+// @description Temporary workaround to get your userscripts working again when you are missing the trailing slash in the URL
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/].*)?$/
-// @version     1.0
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
-if (window.location.pathname.indexOf('editor/') == -1) {
-  window.location = window.location.href.replace('editor', 'editor/');
+if (window.location.pathname.indexOf('editor/') == -1 && window.location != window.location.href.replace('/editor', '/editor/')) {
+  window.location = window.location.href.replace('/editor', '/editor/');
 }
