3534ae55df86240fec5209b611f449a6ea44ca47
Glodenox <github@tomputtemans.com>
2018-03-07 16:13:29 +0100

    Added a timeout of 1 second to prevent HTTP 500 error

--- wme-url-fixer.user.js
+++ wme-url-fixer.user.js
@@ -3,10 +3,12 @@
 // @namespace   http://www.tomputtemans.com/
 // @description Temporary workaround to get your userscripts working again when you are missing the trailing slash in the URL
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/].*)?$/
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
 if (window.location.pathname.indexOf('editor/') == -1 && window.location != window.location.href.replace('/editor', '/editor/')) {
-  window.location = window.location.href.replace('/editor', '/editor/');
+  setTimeout(function() {
+    window.location = window.location.href.replace('/editor', '/editor/');
+  }, 1000);
 }
