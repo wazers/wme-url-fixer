b7c605dc3093e6c2254c6ec697fd4ce52abb4f89
Glodenox <github@tomputtemans.com>
2018-03-25 15:11:33 +0200

    Apparently it still causes issues on some slow computers, timeout increased

--- wme-url-fixer.user.js
+++ wme-url-fixer.user.js
@@ -3,12 +3,12 @@
 // @namespace   http://www.tomputtemans.com/
 // @description Temporary workaround to get your userscripts working again when you are missing the trailing slash in the URL
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/].*)?$/
-// @version     1.2
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 
 if (window.location.pathname.indexOf('editor/') == -1 && window.location != window.location.href.replace('/editor', '/editor/')) {
   setTimeout(function() {
     window.location = window.location.href.replace('/editor', '/editor/');
-  }, 1000);
+  }, 5000);
 }
